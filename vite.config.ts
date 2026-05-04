e52d742d41a27cdb94867ebc7f5067f966dc9dd3
FarhanGits <daffafarhan05@gmail.com>
2026-05-04 16:02:30 +0700

    rollback configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: process.env.DEPLOY_TARGET === 'github' ? '/portfolio/' : '/', // '/portfolio' untuk github pages, '/' untuk cloudflare pages
+  base: process.env.DEPLOY_TARGET === 'github' ? '/portfolio/' : '/', // '/portfolio' untuk github pages, '/' untuk default deploy
   build: {
-    outDir: process.env.DEPLOY_TARGET === 'github' ? 'docs' : 'dist' // '/docs' untuk github pages, '/dist' untuk cloudflare pages
+    outDir: process.env.DEPLOY_TARGET === 'github' ? 'docs' : 'dist' // '/docs' untuk github pages, '/dist' untuk default deploy
   }
 }))
 // Run "npm run build:github" untuk deploy via github pages sebelum push & merge ke main
